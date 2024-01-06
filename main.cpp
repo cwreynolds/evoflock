bd9d010b81a33b1f06be78887e95ac9a7649a6f4
Craig Reynolds <cwr@red3d.com>
2024-01-06 12:18:40 -0800

    Add main.cpp and Xcode project.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  evoflock
+//
+//  Created by Craig Reynolds on 1/6/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
